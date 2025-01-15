379a0b36809948c3a539023ff3825007e34793a2
DerickFrias <92408574+DerickFrias@users.noreply.github.com>
2025-01-15 18:07:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@
 let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
 clickCountDisplay.textContent = clickCount;
         updateUI();
-
+//The event listener increments the click count every time the button is clicked. It updates the localstorage with the new count, updates the displayed count on the website and calls the updateUI function to change the background color.
 button.addEventListener('click', () => {
     clickCount++;
     localStorage.setItem('clickCount', clickCount);
     clickCountDisplay.textContent = clickCount;
     updateUI();
 });
-
+// The updateUI function changes the color and text after each click. It randomly selects a color from the color array for the background and a message from the texts array for the text.
 function updateUI() {
     const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#E91E63'];
     const texts = [
