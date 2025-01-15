5d6656ddabd9d0631eba58ce81eeaf22878038d8
DerickFrias <92408574+DerickFrias@users.noreply.github.com>
2025-01-15 18:03:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
  const clickCountDisplay = document.getElementById('click-count');
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
+// the let click count variable stores the current number of button clicks. Then it gets the count form local storage if its available, if there is no count in localstorage it initializes to 0 
 let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
-
 clickCountDisplay.textContent = clickCount;
         updateUI();
 
