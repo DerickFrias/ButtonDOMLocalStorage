6bea8e4b08c9e71d31f2c89c2aa4627a80602f9d
DerickFrias <92408574+DerickFrias@users.noreply.github.com>
2025-01-15 17:44:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
  const clickCountDisplay = document.getElementById('click-count');
         const header = document.getElementById('header');
         const button = document.getElementById('click-button');
+let clickCount = localStorage.getItem('clickCount') ? parseInt(localStorage.getItem('clickCount')) : 0;
+clickCountDisplay.textContent = clickCount;
+        updateUI();
